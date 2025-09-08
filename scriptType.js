d9238e81b6fc8aa63adb805bd89fa032e3e4cc00
monisree81 <167197671+monisree81@users.noreply.github.com>
2025-09-08 10:32:09 +0530

    Create scriptType.js

---
+++ scriptType.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",(event)=>{ 
+document.getElementById("highlightBtn").addEventListener("click",()=>{ 
+const btnclass=document.getElementById("textPara"); 
+btnclass.classList.toggle("highlight"); 
+}) 
+}) 
